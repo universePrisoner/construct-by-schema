0f6085c7811c446c7a1cf9af47dd00890d0ac53b
A. Volgin <anatoly.volgin.rg@gmail.com>
2022-02-01 01:07:34 +0300

    chore: add prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { hot } from 'react-hot-loader/root';
 import * as React from 'react';
 
-function App() { return <h1> Starting Point</h1>; }
+function App() {
+	return <h1> Starting Point</h1>;
+}
 
 export default hot(App);
