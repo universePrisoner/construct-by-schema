0a6d317a32a8f55530d0731f03048b6d9c44f8d3
A. Volgin <anatoly.volgin.rg@gmail.com>
2022-02-01 03:34:08 +0300

    chore: additional configuration for primitive setup

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
 	plugins: [
 		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, '/templates/index.html'),
+			template: path.resolve(__dirname, './templates/index.html'),
 		}),
 	],
 };
